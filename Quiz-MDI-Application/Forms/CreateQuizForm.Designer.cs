0325fe46a547e9bbeaad5eaf8f61bf7a8c273ee1
antoniyaat <antoniyaata@gmail.com>
2019-05-19 18:28:17 +0300

    Added [Create Quiz] form

---
+++ Quiz-MDI-Application/Forms/CreateQuizForm.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace Quiz_MDI_Application.Forms
+{
+    partial class CreateQuizForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CreateQuizForm));
+            this.SuspendLayout();
+            // 
+            // CreateQuizForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(35F, 82F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Quiz_MDI_Application.Properties.Resources._17580;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1104, 664);
+            this.Font = new System.Drawing.Font("Calibri", 40.2F, System.Drawing.FontStyle.Bold);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(13, 15, 13, 15);
+            this.Name = "CreateQuizForm";
+            this.Text = "Create Quiz";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
